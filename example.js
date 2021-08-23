77e25d55820360b882a4775b0e9fb897c32f3480
Simon Lee <simon.lee@cardinalblue.com>
2021-08-23 21:47:53 +0800

    Updated project structure

---
+++ example.js
@@ -0,0 +1,20 @@
+const StoreKit = require('./storekit');
+
+(async () => {
+  const store = new StoreKit({
+    baseURL: 'https://api.storekit-sandbox.itunes.apple.com/inApps/v1',
+    issuer: '69a6de70-ff9b-47e3-e053-5b8c7c11a4d1',
+    bid: 'com.cardinalblue.PicCollage',
+    kid: '6R27A8XG4T',
+    privateKeyPath: 'SubscriptionKey_6R27A8XG4T.p8',
+  });
+
+  const subscriptions = await store.subscriptions(1000000459891047);
+  const history = await store.history(1000000459891047);
+  console.log(subscriptions);
+  console.log(history);
+})();
+
+module.exports = {
+  StoreKit,
+};
